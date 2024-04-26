1e426105a0b43629d9a0ec3e5b3e7bd589b13e4d
Blaaaxo <fernandezb@tecnocomfenalco.edu.co>
2024-04-26 01:56:47 -0500

    crud terminado

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,22 @@ public class StudentController {
     @GetMapping("/")
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping("/create")
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "/delete/{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "/update/{studentId}")
+    public void updateStudent(@PathVariable Long studentId,
+                              @RequestParam(required = false) String name,
+                              @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,10 +2,11 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -17,7 +18,77 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
+    //funcion que trae a todos los estudiantes de la base de datos
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    //funcion que agrega nuevos estudiantes a la base de datos
+    public void addNewStudent(Student student) {
+
+        //verifica si el email ya esta en uso
+        Optional<Student> studentOptional = studentRepository.
+                findStudentByEmail(student.getEmail());
+
+        //si el email ya esta en uso lanza una excepcion
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+
+        //si no esta en uso guarda al estudiante
+        studentRepository.save(student);
+    }
+
+    //funcion que elimina estudiantes de la base de datos
+    public void deleteStudent(Long studentId) {
+
+        //verifica si el estudiante existe
+        boolean exists = studentRepository.existsById(studentId);
+
+        //si no existe lanza una excepcion
+        if (!exists){
+            throw new IllegalStateException(
+                    "student with id " + studentId + " does not exists"
+            );
+        }
+
+        //elimina al estudiante
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email) {
+
+        //verifica si el estudiante existe
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id " + studentId + " does not exists"
+                ));
+
+        //si el nombre no es nulo y no es igual al que ya tiene lo actualiza
+        if (name != null &&
+                name.length() > 0 &&
+                !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        //si el email no es nulo y no es igual al que ya tiene lo actualiza
+        if (email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)){
+
+            //verifica si el email ya esta en uso
+            Optional<Student> studentOptional = studentRepository.
+                    findStudentByEmail(email);
+
+            //si el email ya esta en uso lanza una excepcion
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+
+            student.setEmail(email);
+        }
     }
 }
