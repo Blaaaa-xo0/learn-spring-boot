88f565cf76aea640eb223b034824721846745085
Blaaaxo <fernandezb@tecnocomfenalco.edu.co>
2024-04-25 22:35:52 -0500

    student service add

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,25 @@
 package com.example.demo.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+
+    /* @Id: esta anotacion indica que el campo es la
+     llave primaria y debe tener una secuencia */
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
